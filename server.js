42897e6b42d04c2e35d562a0511e44f11282edb0
Victor <>
2024-09-01 02:15:02 +0530

    test

--- server.js
+++ server.js
@@ -1,53 +1,27 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const http = require('http');
 const puppeteer = require('puppeteer');
-const fs = require('fs');
-const path = require('path');
 
-const app = express();
-const port = process.env.PORT || 3000;
+const server = http.createServer(async (req, res) => {
+    let message = 'It works!\n';
+    let version = 'NodeJS ' + process.versions.node + '\n';
+    let puppeteerStatus = 'Puppeteer status: ';
 
-app.use(bodyParser.json());
+    try {
+        // Attempt to launch a browser instance to verify Puppeteer is working
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox', '--disable-setuid-sandbox'],
+        });
+        await browser.close();
+        puppeteerStatus += 'Installed and working!';
+    } catch (error) {
+        puppeteerStatus += 'Not installed or not working.';
+    }
 
-app.post('/convert', async (req, res) => {
-  const { html, css } = req.body;
-
-  if (!html) {
-    return res.status(400).send('HTML content is required');
-  }
-
-  try {
-    // Launch Puppeteer
-    const browser = await puppeteer.launch({
-      args: ['--no-sandbox', '--disable-setuid-sandbox'],
-    });
-    const page = await browser.newPage();
-
-    // Set the content
-    await page.setContent(`
-      <html>
-        <head>
-          <style>${css}</style>
-        </head>
-        <body>${html}</body>
-      </html>
-    `);
-
-    // Capture screenshot
-    const screenshotPath = path.join(__dirname, 'screenshot.png');
-    await page.screenshot({ path: screenshotPath });
-
-    await browser.close();
-
-    // Send the screenshot
-    res.sendFile(screenshotPath, () => {
-      fs.unlinkSync(screenshotPath); // Clean up the file after sending
-    });
-  } catch (error) {
-    res.status(500).send('Error generating image');
-  }
+    const response = [message, version, puppeteerStatus].join('\n');
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end(response);
 });
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+server.listen(3000, () => {
+    console.log('Server running on port 3000');
 });
