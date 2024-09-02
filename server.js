c3af298673778dd10054c4a0b0c747a9043e63d9
Victor <>
2024-09-02 18:10:08 +0530

    update

--- server.js
+++ server.js
@@ -1,27 +1,11 @@
-const http = require('http');
-const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
+const port = 3000;
 
-const server = http.createServer(async (req, res) => {
-    let message = 'It works!\n';
-    let version = 'NodeJS ' + process.versions.node + '\n';
-    let puppeteerStatus = 'Puppeteer status: ';
-
-    try {
-        // Attempt to launch a browser instance to verify Puppeteer is working
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox'],
-        });
-        await browser.close();
-        puppeteerStatus += 'Installed and working!';
-    } catch (error) {
-        puppeteerStatus += 'Not installed or not working.';
-    }
-
-    const response = [message, version, puppeteerStatus].join('\n');
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end(response);
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-server.listen(3000, () => {
-    console.log('Server running on port 3000');
+app.listen(port, () => {
+  console.log(`App listening at http://localhost:${port}`);
 });
